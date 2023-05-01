ca9f5ead372d2178a4dfdc0a651ac2347b87dd85
SouthinkGaming <southink419@gmail.com>
2023-05-01 11:37:00 -0400

    Added a few things
    Added a class called ShowingCollection and added a few lines of code to CinemaBookingSystem as well as updating bluej package

--- CinemaBookingSystem.java
+++ CinemaBookingSystem.java
@@ -1,22 +1,23 @@
+import java.util.*;
 
 /**
- * Write a description of class CinemaBookingSystem here.
+ * This is the overall system of the Cinema Booking fianal project. It will hold/call methods & classes named:
+ * Showing Collections, Showing, Theater, Customer, & Showing/theater map
  *
- * @author (your name)
- * @version (a version number or a date)
+ * Artemis MacDuffie, Ryan Connell, & Tara Nordmann
+ * 5/1/2023
  */
-public class CinemaBookingSystem
-{
-    // instance variables - replace the example below with your own
-    
+public class CinemaBookingSystem {
+    // instance variables
+    private ShowingCollection amount;
+    private Showing showing; // showing is place holder for now
+    private Theater status; // used status here because I thought Cinema would check if theater can show another movie or not
 
     /**
      * Constructor for objects of class CinemaBookingSystem
      */
-    public CinemaBookingSystem()
-    {
+    public static void CinemaBookingSystem(String[] args) {
         // initialise instance variables
-        
     }
 
     /**
@@ -25,8 +26,5 @@ public class CinemaBookingSystem
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    // public int sampleMethod(int y)
-    // {
-
-    // }
+    // public int sampleMethod(int y) {}
 }
---
+++ ShowingCollection.java
@@ -0,0 +1,16 @@
+/**
+ * Collection of movies that can be shown at the movie theater
+ *
+ * @author (your name)
+ * 5/1/2023
+ */
+public class ShowingCollection {
+    // instance variables
+
+    /**
+     * Constructor for objects of class ShowingCollection
+     */
+    public ShowingCollection() {
+        // initialise instance variables
+    }
+}
